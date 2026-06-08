34739d895890fe9353ddfdcc04002e928247d114
Pragya Malaiya <pragyamalaiya@gmail.com>
2026-06-08 13:58:33 +0530

    prettier added

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import eslintConfigPrettier from "eslint-config-prettier"
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -18,5 +19,5 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
     },
-  },
+  },eslintConfigPrettier,
 ])
